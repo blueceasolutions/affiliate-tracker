a702a95275aa0146bf79a1faba3a0aa1bec116c5
giprim <gi4real.gi@gmail.com>
2026-02-21 13:17:04 +0100

    chore: delete migration file

--- supabase/migrations/20260220174836_add_public_link_policy.sql
+++
@@ -1,4 +0,0 @@
--- Allow public access to read affiliate links by their unique code
-CREATE POLICY "Public can view affiliate links for tracking"
-  ON public.affiliate_links FOR SELECT
-  USING ( true );
